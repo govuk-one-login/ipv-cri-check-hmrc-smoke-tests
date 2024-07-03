ebf4c4762860503858448894805b641e3ce1c080
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2024-07-03 14:59:20 +0100

    OJ-2543: Improve checks in the test script
    - Check the array length when retrieving canary names from the stack
    - Use jq to verify the lambda invocation didn't return an error
    - Check the boolean value from lambda output to determine if the canary has passed
    
    - Log a progress message

--- test-image/run-tests.sh
+++ test-image/run-tests.sh
@@ -2,28 +2,37 @@
 set -eu
 
 stack_name=${STACK_NAME:-smoke-tests}
+lambda_name="$stack_name-CanaryRunnerFunction"
 
-stack_outputs=$(aws cloudformation describe-stacks --stack-name "$stack_name" --output json | jq '.Stacks[0].Outputs[]')
-lambda_name=$(jq --raw-output 'select(.OutputKey == "CanaryInvokerFunctionName").OutputValue' <<< "$stack_outputs")
+canary_names=$(
+  aws cloudformation describe-stacks --stack-name "$stack_name" --output json \
+    --query 'Stacks[0].Outputs[?OutputKey==`CanaryNames`]'
+)
 
-read -ra canaries < <(jq --raw-output 'select(.OutputKey == "CanaryNames").OutputValue' <<< "$stack_outputs")
+if [[ $(jq length <<< "$canary_names") -eq 0 ]]; then
+  echo "  𝑥 Canary names output not found in stack $stack_name"
+  exit 1
+fi
 
-mkdir -p canary-results
-success=true
+IFS="," read -ra canaries < <(jq --raw-output '.[0].OutputValue' <<< "$canary_names")
+
+canary_results_dir="canary-results"
+mkdir -p "$canary_results_dir"
 
 echo "ℹ Running ${#canaries[@]} canaries from stack $stack_name using function $lambda_name"
+success=true
+index=1
 
 for canary in "${canaries[@]}"; do
-  echo "» Executing canary $canary"
+  echo "» [$((index++))/${#canaries[@]}] Running canary $canary"
 
-  output_file="canary-results/$canary.json"
-  payload=$(jq --null-input --compact-output --arg canaryName "$canary" '{canaryName: $canaryName}' | base64)
+  output_file="$canary_results_dir/$canary.json"
+  payload=$(jq --null-input --arg canaryName "$canary" '{canaryName: $canaryName}' | base64)
+  canary_result=$(aws lambda invoke --function-name "$lambda_name" --payload "$payload" "$output_file")
 
-  invocation_result=$(aws lambda invoke --function-name "$lambda_name" --payload "$payload" "$output_file")
-
-  if [[ $(jq --raw-output '.FunctionError' <<< "$invocation_result") != null ]]; then
-    echo "  𝑥 Lambda invocation failed for $canary"
-    jq . "$output_file"
+  if [[ $(jq 'has("FunctionError")' <<< "$canary_result") == true ]]; then
+    echo "  𝑥 Lambda invocation failed for canary $canary"
+    jq < "$output_file"
     success=false
   elif [[ $(jq --raw-output '.passed' "$output_file") == true ]]; then
     echo "  ✔ Canary $canary passed"
